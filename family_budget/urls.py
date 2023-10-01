0a0c44c92ce3bab2680214cf58c973211c6b3b58
bartwroblewski <barti.wroblewski@gmail.com>
2023-10-01 11:31:19 +0200

    minor url tweaks

--- family_budget/urls.py
+++ family_budget/urls.py
@@ -21,11 +21,11 @@ from rest_framework import routers
 from budget import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'api/users', views.UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('budget.urls')),
     path('register/', views.RegisterUser.as_view()),
