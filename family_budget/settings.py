a7c6682468fc037aa0711facb73e11f4bb39fd66
bartwroblewski <barti.wroblewski@gmail.com>
2023-10-01 20:03:28 +0200

    adds dev docker-compose

--- family_budget/settings.py
+++ family_budget/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'family_budget.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/mysite.sqlite3',
     }
 }
 
